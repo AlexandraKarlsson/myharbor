08a21fa6e9d65f41968025fdb1267424df0a0652
Alexandra Karlsson <betruebefree@gmail.com>
2018-05-31 11:43:42 +0200

    Highlighting active page done + added alt-text to img

--- js/script.js
+++ js/script.js
@@ -11,9 +11,14 @@
             nav.classList.remove("sticky");
         }
     });
-    //window.addEventListener('scroll', fixedNav);
-   // window.onscroll = function() {fixedNav()};
+    
+    var currentLocation = window.location.href;
+    var links = nav.querySelectorAll('a');
+    for(var i = 0; i < links.length; i++) {
+        if(links[i].href == currentLocation) {
+            links[i].parentElement.setAttribute('class', 'activePage');
+        }
+    }
 
 })();
-
 
