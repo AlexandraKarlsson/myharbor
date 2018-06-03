3bf2e2065dae4e28d2664ff28917d01895f35fc3
Alexandra Karlsson <betruebefree@gmail.com>
2018-06-03 15:39:35 +0200

    Comment the code and answered some questions

--- js/script.js
+++ js/script.js
@@ -22,3 +22,9 @@
 
 })();
 
+
+/*
+Genom att jag använder mig av en IIFE så förhindrar jag min kod att läcka ut i det globala scopet, vilket minimerar riskerna att det uppstår namnkonflikt mellan bl.a. variabler.
+Jag anser att min js-kod nästan är produktionsredo, men för att den skall bli fullt produktionsfärdig krävs det en del felhantering utifall någon variabel sätts till null eller undefined.
+
+*/
